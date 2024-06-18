027b1a00ea1b79332076f99c81b19259aa4eeff8
silgurlekian <sgurlekian@gmail.com>
2024-06-18 13:07:27 -0300

    feat: precaching

--- service-worker.js
+++ service-worker.js
@@ -5,10 +5,40 @@ const LISTA = [
     'css/materialize.min.css',
 ];
 
+// Instalación del SW
 self.addEventListener('install', (e) => {
-    console.log("install");
-    const cache = caches.open('MoviesApp').then((cache) => {
-        cache.addAll(LISTA);
-    });
-    e.waitUntil(cache);
+    console.log("Instalación del Service Worker");
+    
+    e.waitUntil(
+        caches.open('MoviesApp').then((cache) => {
+            return cache.addAll(LISTA);
+        })
+    );
+});
+
+// Activación del SW
+self.addEventListener('activate', (e) => {
+    console.log('Activación del Service Worker');
+
+    e.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== 'MoviesApp') {
+                        console.log('Limpiando caché antigua:', cacheName);
+                        return caches.delete(cacheName); // Elimina caché antigua
+                    }
+                })
+            );
+        })
+    );
+});
+
+// Fetch para manejar las solicitudes de red
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then((response) => {
+            return response || fetch(e.request);
+        })
+    );
 });
